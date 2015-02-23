f1013ff1b37310c0e8f63d52c5bae0be4d6012ce
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 14:22:51 +0000

    Added ex13.rb and question.txt

---
+++ ex13.rb
@@ -0,0 +1,17 @@
+
+# a_variable, another_variable = ARGV
+
+# puts "Your first variable is: #{a_variable}"
+# puts "Your second variable is: #{second}"
+# puts "Your third variable is: #{third}"
+
+arg1 = ARGV[0] # arg1, = ARGV would also work
+
+puts "This is the input from the argument: #{arg1}"
+
+# arg2, arg3, arg4, arg5 = ARGV
+
+# puts "This is the input from the argument: #{arg2 + arg3 + arg4 + arg5}"
+
+
+
