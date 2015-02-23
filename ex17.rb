251060df07123dc01bad968154958d52dc1bb215
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 17:02:30 +0000

    Changes after trying to reduce ex17.rb to one file

--- ex17.rb
+++ ex17.rb
@@ -1,20 +1,6 @@
-from_file, to_file = ARGV
+open(ARGV[1], 'w').write(open(ARGV[0]).read);(ARGV[1]).close
 
-puts "Copying from #{from_file} to #{to_file}"
-# we could do these two on one line, how?
-in_file = open(from_file).read
-indata = in_file.read
 
-puts "The input file is #{indata.length} bytes long"
-
-puts "Does the output file exist? #{File.exist?(to_file)}"
-# puts "Ready, hit RETURN to continue, CTRL-C to abort."
-# $stdin.gets
-
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-puts "Alright, all done."
-
-out_file.close
-in_file.close
+file1=open(ARGV[1], 'w');file2=open(ARGV[0]);file1.write(file2.read);file1.close;file2.close
+#out_file.close
+#in_file.close
