7f658ebed465244c11b096cd3740ea442b97173d
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 10:43:23 +0000

    Changed all to floating point

--- ex3.rb
+++ ex3.rb
@@ -3,28 +3,28 @@ puts "I will now count my chickens:"
 #Print Hens calc to screen 30/6 before +25
 puts "Hens #{25.0+30/6}"
 #Print Roosters order 25*3 then mod 4 then 100 -
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25 * 3 % 4}"
 #Print the line of text
 puts "Now I will count the eggs:"
 # Prints just the calculation to the screen.
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 #Prints only as text as the calc is not inside a #{}
 puts "Is it true that 3 + 2 < 5 - 7"
 # Prints the result of a math problem in integer format.
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2 < 5 - 7
 
 # Prints some text with the result of a math some interpolated into the string.
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3.0 + 2}"
 # Prints some text with the result from a math some interpolated into string.
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5 - 7? #{5.0 - 7}"
 # Prints some text to the screen.
 puts "Oh, that's why it's false"
 #prints some text to the screen.
 puts "How about some more."
 # Prints some text and result of finding whether 5 > -2, using string interpolation.
-puts "Is it greater? #{5 > -2}"
+puts "Is it greater? #{5.0 > -2}"
 # Prints some text and uses string interpolation to find and print the boolean result of a math question.
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2}"
 # Prints some text and the result of finding whether 5 less than or equal to minus 2.
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2}"
 
