b7309e4984f7cc21ec2e6703e38533da765af08b
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 11:42:21 +0000

    Commented the first half of ex6.rb

--- ex6.rb
+++ ex6.rb
@@ -1,10 +1,17 @@
+# Declare the types of people variable = 10
 types_of_people = 10
+# Declare the x variable = to a string with the types of people variable inside it
 x = "There are #{types_of_people} types of people."
+# Declare the binary variable = 10
 binary = "binary"
+# Declare the do_not variable = don't
 do_not = "don't"
+# Declare the y variable = to a string with binary and do_not in
 y = "Those who know #{binary} and those who #{do_not}."
 
+#Print x to the screen
 puts x
+#Print y to the screen
 puts y
 
 puts "I said: #{x}."
