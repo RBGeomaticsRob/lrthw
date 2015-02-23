889e7c9b5db242daed05236183c7263891fd60a5
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 18:25:49 +0000

    Commented on top half of ex20.rb

--- ex20.rb
+++ ex20.rb
@@ -1,13 +1,14 @@
+#Set input_file = to the first item of the ARGV array
 input_file = ARGV.first
-
+#Define method print all which takes in a single agument (a file object) and reads the contents of the file
 def print_all(f)
   puts f.read
 end
-
+# Define a method which sets the location value of the file argument back to 0 (or the start of the file)
 def rewind(f)
   f.seek(0)
 end
-
+# Define a method that prints the line count value as the first argument and print the next line of f depending on the value that seek() points towards.
 def print_a_line(line_count, f)
   puts "#{line_count}, #{f.gets.chomp}"
 end
