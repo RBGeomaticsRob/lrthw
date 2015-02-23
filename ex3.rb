63a3653bfde45ed9a2075e1de7028f2a1364f167
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-02-23 10:30:12 +0000

    Added comments to show what the bottom half of ex3.rb does

--- ex3.rb
+++ ex3.rb
@@ -8,16 +8,20 @@ puts "Now I will count the eggs:"
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 puts "Is it true that 3 + 2 < 5 - 7"
-
+# Prints the result of a math problem in integer format.
 puts 3 + 2 < 5 - 7
 
+# Prints some text with the result of a math some interpolated into the string.
 puts "What is 3 + 2? #{3 + 2}"
+# Prints some text with the result from a math some interpolated into string.
 puts "What is 5 - 7? #{5 - 7}"
-
+# Prints some text to the screen.
 puts "Oh, that's why it's false"
-
+#prints some text to the screen.
 puts "How about some more."
-
+# Prints some text and result of finding whether 5 > -2, using string interpolation.
 puts "Is it greater? #{5 > -2}"
+# Prints some text and uses string interpolation to find and print the boolean result of a math question.
 puts "Is it greater or equal? #{5 >= -2}"
+# Prints some text and the result of finding whether 5 less than or equal to minus 2.
 puts "Is it less or equal? #{5 <= -2}"
