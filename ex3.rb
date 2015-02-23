d0b7d2c7eefdf0973b574e469d5ca5fc22a2356f
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 10:29:29 +0000

    Commented the first part

--- ex3.rb
+++ ex3.rb
@@ -1,12 +1,14 @@
+#Print the line of text to the screen
 puts "I will now count my chickens:"
-
+#Print Hens calc to screen 30/6 before +25
 puts "Hens #{25+30/6}"
+#Print Roosters order 25*3 then mod 4 then 100 -
 puts "Roosters #{100 - 25 * 3 % 4}"
-
+#Print the line of text
 puts "Now I will count the eggs:"
-
+# Prints just the calculation to the screen.
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+#Prints only as text as the calc is not inside a #{}
 puts "Is it true that 3 + 2 < 5 - 7"
 
 puts 3 + 2 < 5 - 7
