b7f0bb3116088052663bafddc130fd53a8f225d0
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-02-23 11:40:49 +0000

    Added comments for the second half of ex6.rb explaining code

--- ex6.rb
+++ ex6.rb
@@ -7,15 +7,23 @@ y = "Those who know #{binary} and those who #{do_not}."
 puts x
 puts y
 
+# prints a string with interpolated variable
 puts "I said: #{x}."
+# prints a string with interpolated variable
 puts "I also said: '#{y}'."
 
+#sets a variable to equal a boolean
 hilarious = false
+#prints a string with an interpolated boolean
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# Prints a variable that is contains to a string
 puts joke_evaluation
 
+# sets a variable to equal a string
 w = "This is the left side of..."
+# sets a variable to equal a string
 e = "a string with a right side."
 
+# prints the result of adding two strings
 puts w + e
