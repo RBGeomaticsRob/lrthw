90f9a231a82b2e17daf5b027ad1f8c5602ce4ac0
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-02-23 17:44:12 +0000

    Added comments to bottom half of ex19.rb

--- ex19.rb
+++ ex19.rb
@@ -7,16 +7,21 @@ end
 
 puts "We can just give the function numbers directly:"
 cheese_and_crackers(20,30)
-
+#prints a string to the screen
 puts "OR, we can use variables from our script:"
 
+#sets a variable to 10
 amount_of_cheese = 10
+#sets a variable to 50
 amount_of_crackers = 50
 
+#Calls a method using two variables as arguments.
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
+#Prints a string and a method. Arguments for the method are the result of two sums.
 puts "We can even do the math inside too: "
 cheese_and_crackers(10+20, 5+6)
 
+#Prints a string and the result of a method (Methods arguments are are variables plus integers)
 puts "And we can combine the two, variables and math: "
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
