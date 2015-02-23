cba5589e51b4774fb647f5fd25c5ad925d724b33
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-02-23 18:25:52 +0000

    Added comments to second half of ex20.rb

--- ex20.rb
+++ ex20.rb
@@ -11,24 +11,31 @@ end
 def print_a_line(line_count, f)
   puts "#{line_count}, #{f.gets.chomp}"
 end
-
+# sets an opened file object to a variable.
 current_file = open(input_file)
-
+# prints a string
 puts "First let's print the whole file:\n"
-
+#calls a function with a variable
 print_all(current_file)
-
+#prints a string
 puts "Now let's rewind, kind of like a tape."
 
+#calls a function with a variable
 rewind(current_file)
-
+#prints a string
 puts "Let's print three lines:"
 
+#sets a variable to an integer
 current_line = 1
+#calls a function with two variables
 print_a_line(current_line, current_file)
 
+#sets a variable to equal a variable + 1
 current_line = current_line + 1
+#Calls a function with two variables
 print_a_line(current_line, current_file)
 
+# Sets variable to a variable plus one
 current_line = current_line + 1
+# Calls the print a line function with two variables
 print_a_line(current_line, current_file)
