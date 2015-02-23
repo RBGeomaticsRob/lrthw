bd8b7467ce632bdb4967c5b0e2150ee811cbfee4
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-02-23 17:44:44 +0000

    Added first half of comments to ex19.rb

--- ex19.rb
+++ ex19.rb
@@ -1,11 +1,17 @@
+#create a function called cheese_and_crackers with two arguments
 def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  # Print a string to the screen with the first argument
   puts "You have #{cheese_count} cheeses!"
+  # Print a string to the screen with the second argument
   puts "You have #{boxes_of_crackers} boxes of crackers!"
+  # Print a string to the screen
   puts "Man that's enough for a party!"
+  #Print a string to the screen with a second line break
   puts "Get a blanket.\n"
 end
-
+#Print a string to the screen
 puts "We can just give the function numbers directly:"
+#Output the response to sending 20 and 30 as arguments to cheese_and_crackers
 cheese_and_crackers(20,30)
 
 puts "OR, we can use variables from our script:"
