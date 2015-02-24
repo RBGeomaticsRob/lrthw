55023034333611f5492d7c6cd5d445070678a308
Jade King <dev@jadeking.co.uk>
2015-02-24 15:14:58 +0000

    Updates ex30 comments

--- ex30.rb
+++ ex30.rb
@@ -7,20 +7,21 @@ if cars > people
 elsif cars < people
 	puts "We should not take th cars"
 else
-	puts
-	"We can't decide"
+	puts "We can't decide"
 end
 
-if trucks > cars
+if trucks > cars # if trusk are more than cars
 	puts "That's too many trucks"
-elsif trucks < cars
+elsif trucks < cars #if trucks are less than cars
 	puts "Maybe we could take the trucks"
-else
+else #If above IF and ELSIF are false run else statement
 	puts "We still can't decide"
 end
 
-if people >= trucks || cars > people
+# if people are more than or equal to cars OR 
+#cars are more than people
+if people >= trucks || cars > people 
 	puts "Alright lets take the trucks"
-else
-	puts "Fine, lets stay at home then."
+else #If above IF is false run else statement
+	puts "Fine, lets stay at home then." 
 end
